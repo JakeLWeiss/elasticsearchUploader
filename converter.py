fab6ea89deca82227f880a9dacd9c4c480202d17
Jake Weiss <32331702+JakeLWeiss@users.noreply.github.com>
2020-02-11 13:20:38 -0500

    added a test document, tweaked a couple of minor elements for indexing

--- converter.py
+++ converter.py
@@ -17,7 +17,5 @@ for i in range(0, pdfreader.numPages):
 #may need utf-8 specified for bytes()
 enc64 = base64.b64encode(bytes(text))
 
-print (enc64)
-
-curlReq = "curl -X PUT \"localhost:9200/documents/_pdf/42?pipeline=attachment\" -H 'Content-Type: application/json' -d' {\"data\": \"" + enc64+ "\"}'"
+curlReq = "curl -X PUT \"localhost:9200/documents/pdf/42?pipeline=attachment&pretty\" -H 'Content-Type: application/json' -d' {\"data\": \"" + enc64+ "\"}'"
 os.system(curlReq)
