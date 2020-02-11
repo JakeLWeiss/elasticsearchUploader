56005758b074315c7ab91842c63e277c004ee0cb
Jake Weiss <32331702+JakeLWeiss@users.noreply.github.com>
2020-02-11 16:51:55 -0500

    added configuration settings and loading for the id

--- converter.py
+++ converter.py
@@ -5,17 +5,98 @@ import sys
 import PyPDF2
 import requests
 import os
+import json
 
-pdf = open(sys.argv[1], 'rb')
-pdfreader = PyPDF2.PdfFileReader(pdf)
+def getID():
+	cfg = None
+	try: #open file to read as a check
+		with open("cfg.json", "r") as f:
+			cfg = json.load(f)
+	
+	except IOError: #if it fails generate the file and put in seed val of 0
+		print("Failed to read cfg.json file. Please run this script with the flag --config to configure settings.")
+		exit()
 
+	id = cfg.get('id') + 1
+	
+	#write updated seed to file
+	with open("cfg.json", "w+") as f:
+		cfg = json.dumps({'host':cfg.get('host'), 'port':cfg.get('port'), 'id': id})
+		f.write(cfg)
+
+	return str(id)
+
+def processPDF():
+	text = ''
+	pdf = open(sys.argv[1], 'rb')
+	pdfreader = PyPDF2.PdfFileReader(pdf)
+
+	for i in range(0, pdfreader.numPages):
+		page = pdfreader.getPage(i)
+		text += page.extractText()
+	return text
+
+def loadSettings():
+	cfg = None
+	try: #open file to read as a check
+		with open("cfg.json", "r") as f:
+			cfg = json.load(f)
+	except IOError: #if it fails generate the file
+		print("Missing cfg.json file, new config file has been created.")
+		cfg = { 'host' : '' , 'port' : 9200 , 'id' : 0 }
+		with open("cfg.json", "w+") as f:
+			f.write(json.dumps(cfg))
+
+	return cfg
+
+def configSettings():
+	cfg = loadSettings()
+
+	host = str(raw_input("Enter host adress: ")) #get new host and check it
+	if(host != ''):
+		print("Host address updated");
+	else:
+		host = cfg.get('host')
+
+	port = None
+	try: #get port or pass if invalid
+		port = int(raw_input("Enter port number: "))
+		print("Port value updated") 
+	except ValueError:
+		port = cfg.get('port')
+
+	seed = None
+	try: #get base seed value or pass if invalid
+		seed = int(raw_input("Enter int for starting seed id: "))
+		print("Base seed value updated") 
+	except ValueError:
+		seed = cfg.get('seed')
+
+	cfg_dict = { 'host' : host , 'port' : port , 'id' : seed }
+	cfg =  json.dumps(cfg_dict)
+
+	with open("cfg.json", "w+") as f:
+		f.write(cfg)
+	
+#-------------------------------------------------------------------------
+doctype = 'pdf' #default type is PDF
 text = ''
-for i in range(0, pdfreader.numPages):
-	page = pdfreader.getPage(i)
-	text += page.extractText()
+cfg = loadSettings();
+
+if(sys.argv[1] == "--config"):
+	configSettings()
+	exit()
+
+elif(sys.argv[1].endswith(".pdf")):
+	text = processPDF()
+	doctype = 'pdf'
+
+id = getID()
 
 #may need utf-8 specified for bytes()
 enc64 = base64.b64encode(bytes(text))
 
-curlReq = "curl -X PUT \"localhost:9200/documents/pdf/42?pipeline=attachment&pretty\" -H 'Content-Type: application/json' -d' {\"data\": \"" + enc64+ "\"}'"
+curlReq = "curl -X PUT \""+cfg.get('host')+":"+str(cfg.get('port'))+"/documents/" + doctype + "/" + str(id) +"?pipeline=attachment&pretty\" -H 'Content-Type: application/json' -d' {\"data\": \"" + enc64+ "\"}'"
+print (curlReq)
 os.system(curlReq)
+
