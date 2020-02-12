74972222a2e522c3bcb0cab209c429dd51b26a6e
root <root@mudkip.localdomain>
2020-02-12 13:04:51 -0500

    added auth settings and functionality for cloud clusters

--- converter.py
+++ converter.py
@@ -9,16 +9,16 @@ import json
 
 def init():
 
-	confirm = str(raw_input("Any existing cfg.json file will be cleared. Continue? ['yes' to contine]  "))
+	confirm = str(raw_input("Any existing cfg.json file will be cleared. Continue? ['yes' to contine]  ")) #confirm making new file
 	if not confirm == "yes":
 		exit()
 
 	print("Making cfg.json file.")
-	cfg = { 'host' : 'localhost' , 'port' : 9200 , 'id' : 0 }
+	cfg = { 'host' : 'localhost' , 'port' : 9200 , 'id' : 0 , 'uname': '', 'pwd': ''} #format and write the default data to json
 	with open("cfg.json", "w+") as f:
 		f.write(json.dumps(cfg))
 
-	configSettings()
+	configSettings() #configure the settings to instance
 
 def getID():
 	cfg = None
@@ -34,36 +34,36 @@ def getID():
 	
 	#write updated seed to file
 	with open("cfg.json", "w+") as f:
-		cfg = json.dumps({'host':cfg.get('host'), 'port':cfg.get('port'), 'id': id})
+		cfg = json.dumps({'host':cfg.get('host'), 'port':cfg.get('port'), 'id': id, 'uname':cfg.get('uname'), 'pwd': cfg.get('pwd') })
 		f.write(cfg)
 
 	return str(id)
 
 def processPDF():
 	text = ''
-	pdf = open(sys.argv[1], 'rb')
+	pdf = open(sys.argv[1], 'rb') #open the pdf file and load it with the PyPDF
 	pdfreader = PyPDF2.PdfFileReader(pdf)
 
-	for i in range(0, pdfreader.numPages):
+	for i in range(0, pdfreader.numPages): #append all pages to the text string
 		page = pdfreader.getPage(i)
 		text += page.extractText()
-	return text
+	return text #return full pdf doc
 
 def loadSettings():
 	cfg = None
 	try: #open file to read as a check
 		with open("cfg.json", "r") as f:
 			cfg = json.load(f)
-	except IOError: #if it fails generate the file
+	except IOError: #if it fails generate the file with defaults for elasticsearch
 		print("Missing cfg.json file, new config file has been created.")
-		cfg = { 'host' : 'localhost' , 'port' : 9200 , 'id' : 0 }
+		cfg = { 'host' : 'localhost' , 'port' : 9200 , 'id' : 0, 'uname': '', 'pwd': '' }
 		with open("cfg.json", "w+") as f:
 			f.write(json.dumps(cfg))
 
 	return cfg
 
 def configSettings():
-	cfg = loadSettings()
+	cfg = loadSettings() #load current settings
 
 	host = str(raw_input("Enter host adress: ")) #get new host and check it
 	if(host != ''):
@@ -78,14 +78,28 @@ def configSettings():
 	except ValueError:
 		port = cfg.get('port')
 
+	uname = str(raw_input("Enter username: ")) #get new uname and check it
+	if(uname != ''):
+		print("Username updated");
+	else:
+		uname = cfg.get('uname')
+
+	pwd = str(raw_input("Enter Password: ")) #get new password and check it
+	if(pwd != ''):
+		print("Password updated");
+	else:
+		pwd = cfg.get('pwd')
+
 	seed = None
 	try: #get base seed value or pass if invalid
 		seed = int(raw_input("Enter int for starting seed id: "))
 		print("Base seed value updated") 
 	except ValueError:
-		seed = cfg.get('seed')
+		seed = cfg.get('id')
 
-	cfg_dict = { 'host' : host , 'port' : port , 'id' : seed }
+
+	#load information into config dict and write it to json file
+	cfg_dict = { 'host' : host , 'port' : port , 'id' : seed, 'uname': uname, 'pwd' : pwd}
 	cfg =  json.dumps(cfg_dict)
 
 	with open("cfg.json", "w+") as f:
@@ -93,31 +107,35 @@ def configSettings():
 	
 #-------------------------------------------------------------------------
 doctype = 'pdf' #default type is PDF
-text = ''
-cfg = loadSettings();
+text = '' #placeholder for text output
+cfg = loadSettings(); #load the cfg.json settings
 
-if(sys.argv[1] == "--config"):
+#get the arg and act with it
+if(sys.argv[1] == "--config"): #configure settings
 	configSettings()
+	exit()
+
+elif(sys.argv[1] == "--init"): #make fresh settings
+	init()
 	exit()
 
 elif(sys.argv[1].endswith(".pdf")):
 	text = processPDF()
 	doctype = 'pdf'
 
-elif(sys.argv[1] == "--init"):
-	init()
-	exit()
-
-else:
+else: #default exit gracefully
 	print("No valid file or command specified.")
 	exit()
 
-id = getID()
+id = getID() #get the current ID to avoid overlap
 
 #may need utf-8 specified for bytes()
-enc64 = base64.b64encode(bytes(text))
+enc64 = base64.b64encode(bytes(text)) #encode pdf in base64
 
-curlReq = "curl -X PUT \""+cfg.get('host')+":"+str(cfg.get('port'))+"/documents/" + doctype + "/" + str(id) +"?pipeline=attachment&pretty\" -H 'Content-Type: application/json' -d' {\"data\": \"" + enc64+ "\"}'"
-print (curlReq)
+curlReq = ''
+#format and make http call to elasticsearch
+if(cfg.get('uname') == '' or cfg.get('pwd') == ''):
+	curlReq = "curl -XPUT \""+cfg.get('host')+":"+str(cfg.get('port'))+"/documents/" + doctype + "/" + str(id) +"?pipeline=attachment&pretty\" -H 'Content-Type: application/json' -d' {\"data\": \"" + enc64+ "\"}'"
+else:
+	curlReq = "curl --user "+cfg.get('uname') + ":" + cfg.get('pwd') + " -XPUT \""+cfg.get('host')+":"+str(cfg.get('port'))+"/documents/" + doctype + "/" + str(id) +"?pipeline=attachment&pretty\" -H 'Content-Type: application/json' -d' {\"data\": \"" + enc64+ "\"}'"
 os.system(curlReq)
-
